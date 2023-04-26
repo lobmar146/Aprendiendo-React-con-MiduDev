932422b1df08bb3c0d7122bed4dab7a472b56f50
lobmar146 <lobmar146@gmail.com>
2023-04-26 16:09:58 -0300

    Agregando States.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
 export function App() {
     return (
-        <>
-            <TwitterFollowCard userName="midudev" name="Miguel Angel Duran" />
-            <TwitterFollowCard userName="martin" name="Martín Lobos" />
-            <TwitterFollowCard userName="luks" name="Lucas Diaz" />
-        </>
+        <section className="App">
+            <TwitterFollowCard isFollowing userName="midudev">
+                Miguel Angel Duran
+            </TwitterFollowCard>
+            <TwitterFollowCard isFollowing={false} userName="lobmar146">
+                Martín Lobos
+            </TwitterFollowCard>
+
+            <TwitterFollowCard isFollowing={false} userName="lucasgustavodiaz">
+                Lucas Diaz
+            </TwitterFollowCard>
+        </section>
     )
 }
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,15 +1,17 @@
-export function TwitterFollowCard({ userName, name, isFollowing }) {
+export function TwitterFollowCard({ children, userName, name, isFollowing }) {
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button isfollowing' : 'tw-followCard-button'
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
                 <img className="tw-followCard-avatar" src={`https://unavatar.io/${userName}`} alt="Avatar de midu" />
                 <div className="tw-followCard-info">
-                    <strong>{name}</strong>
-                    <span className="tw-followCard-infoUserName"> Midu</span>
+                    <strong>{children}</strong>
+                    <span className="tw-followCard-infoUserName"> @{userName}</span>
                 </div>
             </header>
             <aside>
-                <button className="tw-followCard-button"> Seguir</button>
+                <button className={buttonClassName}>{text}</button>
             </aside>
         </article>
     )
